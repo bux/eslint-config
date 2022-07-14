7bf3bbb3a29f36d985b9a8eebc5868c2d7ebc155
bux <github@jonathandavid.de>
2022-07-14 07:40:01 +0200

    fix rules

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,7 +1,10 @@
 module.exports = {
     extends: ["@antfu"],
     rules: {
-        quotes: ["error", "double", { avoidEscape: true }],
-        indent: ["error", 4, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
+        "quotes": ["error", "double", { avoidEscape: true }],
+        "@typescript-eslint/quotes": ["error", "double"],
+
+        "indent": ["error", 4, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
+        "@typescript-eslint/indent": ["error", 4],
     },
 }
